8b07ba30b49a3d5131efde86e42347e70257c552
Emmanuel Mateo Gonzalez Zaragoza <59709622+MateoGonzar@users.noreply.github.com>
2023-11-18 17:41:00 -0600

    Algoritmo BackEnd - Actualizado 17/11/2023

--- D01_Equipo1_Backend.py
+++ D01_Equipo1_Backend.py
@@ -1,19 +1,26 @@
 import math
 
 type Ubicacion = tuple[float, float]
-type Ruta = dict[str, Ubicacion]
+type Ruta = list[object]
 
-class Node:
-    def __init__(self, ubicacion=Ubicacion, left=None, right=None):
+class NodoUbicacion:
+    def __init__(self, peso: int, ubicacion: Ubicacion, anterior: object, siguiente: object) -> None:
+        self.peso = peso
         self.ubicacion = ubicacion
-        self.left = left
-        self.right = right
+        self.anterior = anterior
+        self.siguiente = siguiente
+    
+    def set_anterior(self, anterior: object) -> None:
+        self.anterior = anterior
+    
+    def set_siguiente(self, siguiente: object) -> None:
+        self.siguiente = siguiente
 
 def distancia_euclidiana(u1: Ubicacion, u2: Ubicacion) -> float:
     distancia = math.sqrt((u2[0] - u1[0])**2 + (u2[1] - u1[1])**2)
     return distancia
 
-def matriz(ubicaciones: list[Ubicacion]) -> list:
+def matriz(ubicaciones: tuple) -> list:
     matriz = []
     for i in ubicaciones:
         fila = []
@@ -23,7 +30,36 @@ def matriz(ubicaciones: list[Ubicacion]) -> list:
         matriz.append(fila)
     return matriz
 
-UBICACIONES = [(3.0, 4.0), (1.0, 2.0)]
+def ubicaciones_a_nodos(ubicaciones: tuple) -> list[NodoUbicacion]:
+    paquetes = []
+    peso = 1
+    for ubicacion in ubicaciones:
+        paquete = NodoUbicacion(peso, ubicacion, None, None)
+        paquetes.append(paquete)
+    return paquetes
 
-matriz_1 = matriz(UBICACIONES)
-print(f'{matriz_1}')
+def seleccion_ruta(posibles_ruta_1: Ruta, posibles_ruta_2: Ruta) -> numero_ruta: int, posicion: int:
+    return
+
+def algoritmo(camion1: int, camion2: int, *args: Ubicacion) -> list[Ruta]:
+    ruta_1: Ruta = []
+    ruta_2: Ruta = []
+    matriz_ubicaciones = matriz(args)
+    nodos_ubicacion = ubicaciones_a_nodos(args)
+    ubicaciones_por_visitar = nodos_ubicacion.copy()
+    ubicacion = 0
+    ruta_1.append(nodos_ubicacion[ubicacion])
+    ruta_2.append(nodos_ubicacion[ubicacion])
+    ubicaciones_por_visitar.remove(nodos_ubicacion[ubicacion])
+    while(len(ubicaciones_visitadas) != 0):
+        seleccion_ruta(
+        ruta_1.append(nodos_ubicacion[ubicacion])
+        ruta_2.append(nodos_ubicacion[ubicacion])
+    return nodos_ubicacion
+
+if __name__ == "__main__":
+    CAMION_1 = 10
+    CAMION_2 = 15
+
+    algoritmo_backend = algoritmo(CAMION_1, CAMION_2, (3.0, 4.0), (1.0, 2.0))
+    print(f'{algoritmo_backend}')
